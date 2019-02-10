71cb9df7c7eb2f54f94581a6d3ee05022ca6db1e
Andrew <arhorn@smcm.edu>
2019-02-11 00:35:21 +0700

    basic graph renders

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class Line extends Component {
       var width = 420,
           barHeight = 20;
       
-      var x = d3.scale.linear()
+      var x = d3.scaleLinear()
           .domain([0, d3.max(data)])
           .range([0, width]);
       
@@ -37,7 +37,7 @@ class Line extends Component {
           .attr("height", barHeight - 1);
       
       bar.append("text")
-          .attr("x", function(d) { return x(d) - 3; })
+          .attr("x", function(d) { return x(d) - 8; })
           .attr("y", barHeight / 2)
           .attr("dy", ".35em")
           .text(function(d) { return d; });
