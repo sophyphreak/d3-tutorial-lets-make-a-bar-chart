125ad704ab262d082c74a518b79934b3953a384a
Andrew <arhorn@smcm.edu>
2019-02-10 23:52:02 +0700

    started on pt2

--- src/App.js
+++ src/App.js
@@ -12,13 +12,35 @@ class Line extends Component {
       // D3 Code to create the chart
       // using faux as container
 
-      const data = [4, 8, 15, 16, 23, 42];
+      var data = [4, 8, 15, 16, 23, 42];
 
-      const body = d3.select(faux);
-      const div = body.append("div");
-      div.html("Hello, world!");
-
-
+      var width = 420,
+          barHeight = 20;
+      
+      var x = d3.scale.linear()
+          .domain([0, d3.max(data)])
+          .range([0, width]);
+      
+      var chart = d3.select(faux)
+          .append('svg')
+          .attr("class", "chart")
+          .attr("width", width)
+          .attr("height", barHeight * data.length);
+      
+      var bar = chart.selectAll("g")
+          .data(data)
+        .enter().append("g")
+          .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+      
+      bar.append("rect")
+          .attr("width", x)
+          .attr("height", barHeight - 1);
+      
+      bar.append("text")
+          .attr("x", function(d) { return x(d) - 3; })
+          .attr("y", barHeight / 2)
+          .attr("dy", ".35em")
+          .text(function(d) { return d; });
    }
 
    //     const dataset = [
@@ -80,8 +102,8 @@ class Line extends Component {
    //    svg.append('g')
    //    .attr("transform", `translate(${h - padding}, 0)`)
    //    .call(yAxis);
-   //  }
 
+   //  }
     render() {
       return (
          <div style={{
