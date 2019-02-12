85127367c617a868e07480f4dfaf8580168e4f04
Andrew <arhorn@smcm.edu>
2019-02-12 14:16:35 +0700

    added number labels. bug was using .attr() instead of .text()

--- src/App.js
+++ src/App.js
@@ -12,44 +12,50 @@ class Line extends Component {
     // using faux as container
 
     const data = [
-      {name: "Locke",    value:  4},
-      {name: "Reyes",    value:  8},
-      {name: "Ford",     value: 15},
-      {name: "Jarrah",   value: 16},
-      {name: "Shephard", value: 23},
-      {name: "Kwon",     value: 42}
+      { name: 'Locke', value: 4 },
+      { name: 'Reyes', value: 8 },
+      { name: 'Ford', value: 15 },
+      { name: 'Jarrah', value: 16 },
+      { name: 'Shephard', value: 23 },
+      { name: 'Kwon', value: 42 }
     ];
 
     const width = 960;
     const height = 500;
 
-    const y = d3.scaleLinear()
-          .domain([0, d3.max(data, d => d.value)])
-          .range([height, 0]);
-    
-    const chart = d3.select(faux)
-          .append('svg')
-          .attr('class', 'chart')
-          .attr('width', width)
-          .attr('height', height)
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, d => d.value)])
+      .range([height, 0]);
+
+    const chart = d3
+      .select(faux)
+      .append('svg')
+      .attr('class', 'chart')
+      .attr('width', width)
+      .attr('height', height);
 
     const barWidth = width / data.length;
 
-    const bar = chart.selectAll('g')
-          .data(data)
-        .enter().append('g')
-          .attr('transform', (d, i) => `translate(${i * barWidth})`);
+    const bar = chart
+      .selectAll('g')
+      .data(data)
+      .enter()
+      .append('g')
+      .attr('transform', (d, i) => `translate(${i * barWidth})`);
 
-    bar.append('rect')
-        .attr('y', (d) => y(d.value))
-        .attr('height', d => height - y(d.value))
-        .attr('width', barWidth - 1);
+    bar
+      .append('rect')
+      .attr('y', d => y(d.value))
+      .attr('height', d => height - y(d.value))
+      .attr('width', barWidth - 1);
 
-    bar.append('text')
-        .attr('x', barWidth / 2)
-        .attr('y', d => y(d.value) + 3)
-        .attr('dy', '.75em')
-        .attr(d => d.value)
+    bar
+      .append('text')
+      .attr('x', barWidth / 2)
+      .attr('y', d => y(d.value) + 3)
+      .attr('dy', '.75em')
+      .text(d => d.value);
 
     // const width = 420;
     // const barHeight = 20;
@@ -78,8 +84,6 @@ class Line extends Component {
     //     .attr('y', barHeight / 2)
     //     .attr('dy', '.35em')
     //     .text(d => d.value);
-          
-
   }
 
   //     const dataset = [
